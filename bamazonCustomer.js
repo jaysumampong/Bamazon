961a5754ec9d8b698e93168834e4e22fd9e8be2c
Jay Sumampong <jaysumampong@gmail.com>
2019-10-18 19:29:56 -0600

    wrote first prompt for inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-let msql = require("mysql");
+let mysql = require("mysql");
 let inquirer = require("inquirer");
 
 let connection = mysql.createConnection({
@@ -14,3 +14,19 @@ connection.connect(function(err) {
     console.log("connect id as:" + connection.threadId);
     startPrompt();
 });
+
+function startPrompt() {
+    inquirer.prompt ([{
+        type: "confirm",
+        name: "confirm",
+        message: "Welcome to Bamazon's bakery department ForGoodnessCakes! Would you like to browse our menu?",
+        default: true
+
+    }]).then(function(user) {
+        if (user.confirm === true) {
+            menu();
+        } else {
+            console.log("Thank you for shopping at ForGoodnessCakes! See you back soon!");
+        }
+    })
+}
